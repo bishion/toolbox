6dc412a7bc13598e63170946bdcb9b0d893cf3f1
guofangbi <guofangbi@za-doctor.com>
2022-05-27 11:20:44 +0800

    feat: 脚手架代码以及脚手架demo

---
+++ scaffold-demo/scaffold-demo-starter/src/main/java/cn/bishion/scaffold/demo/ScaffoldDemoApplication.java
@@ -0,0 +1,18 @@
+package cn.bishion.scaffold.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 脚手架演示应用程序
+ *
+ * @author: guofangbi
+ * @date: 2022-05-27 09:06:40
+ * @version: 1.0.0
+ */
+@SpringBootApplication
+public class ScaffoldDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldDemoApplication.class, args);
+    }
+}
